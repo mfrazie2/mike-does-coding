baefb9b8ba941d64105a53962497a235ee6dfd10
Mike Frazier <michael.s.frazier@gmail.com>
2021-07-24 15:02:45 -0600

    Add blog post page to read mdx files

---
+++ src/pages/{Mdx.slug}.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {graphql} from 'gatsby';
+
+const BlogPostPage = ({data}) => {
+
+    return (
+        <h1>{data.mdx.frontmatter.title}</h1>
+    );
+};
+
+export const query = graphql`
+    query BlogPostById($id: String) {
+        mdx(id: {eq: $id}) {
+            frontmatter {
+                title
+            }
+        }
+    }
+`;
+
+export default BlogPostPage;
