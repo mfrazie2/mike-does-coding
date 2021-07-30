24efac4182cb9a422f0dc21787d26a7f6d98c74c
Mike Frazier <michael.s.frazier@gmail.com>
2021-07-30 14:18:48 -0600

    Update index tot be homepage, 404 import fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,24 @@
 import * as React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/Seo";
-import {graphql, Link} from 'gatsby';
 import { StaticImage } from "gatsby-plugin-image";
 
 // markup
-const IndexPage = ({ data }) => {
-  const posts = data.allMdx.nodes;
+const IndexPage = () => {
+
   return (
     <Layout>
-      <SEO title={`Home Page`} />
+      <SEO title={`Home`} />
       <StaticImage 
         src="https://lh3.googleusercontent.com/pw/AM-JKLX72lJj-vodUgH4IEMAwa3l4P_vlW11Bp3HF6t9JnnSVSXOnEgjZjWcK1oU2qopPpgs0ADZzU-q6foIii85H2fTNviICoGAEtNFfic1qDSn8Fa8PHAHy3Hh052U8vZPstIfZioavFqqxD-VnltKv8MT=w2078-h1560-no"
         alt="Winter in the mountains"
         layout="fullWidth"
         aspectRatio={21/9}
       />
-      {posts.map(post => {
-        return (
-          <Link to={post.slug} key={post.id}>
-            <h2>{post.frontmatter.title}</h2>
-            <p>{post.frontmatter.date}</p>
-          </Link>
-        );
-      })}
+      <h1>Welcome to my portfolio</h1>
+      <p>I want this to be like the original Space Jam website.</p>
     </Layout>
   )
 }
-
-export const PageQuery = graphql`
-  query PageQuery {
-    allMdx(sort: {fields: [frontmatter___date], order: DESC}) {
-      nodes {
-        slug
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
-        }
-        id
-      }
-    }
-  }
-`;
 
 export default IndexPage
