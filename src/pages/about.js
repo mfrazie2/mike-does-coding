ceb1c7ada1103d4a289c3a6583f47b5e33052615
Mike Frazier <michael.s.frazier@gmail.com>
2021-07-30 14:18:14 -0600

    Create About page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+
+// markup
+const AboutPage = () => {
+  return (
+    <Layout>
+      <SEO title={`About`} />
+      <div>Legiterally nothing to see here</div>
+    </Layout>
+  )
+}
+
+export default AboutPage
