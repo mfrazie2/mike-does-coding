b70d44f8c6f90b29abb63519d2f4a060d312224b
Mike Frazier <michael.s.frazier@gmail.com>
2021-08-10 20:28:58 -0600

    Create some custom styling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import ResetCss from './resetCss';
+import MikeDoesCodingCss from './MikeDoesCodingCss';
 
 const Layout = ({ children }) => {
   return (
     <>
       <ResetCss />
+      <MikeDoesCodingCss />
       <Header />
       <main>{children}</main>
       <Footer />
---
+++ src/components/MikeDoesCodingCss.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+
+const MikeDoesCodingCss = createGlobalStyle`
+    h1,h2,h3,h4,h5,h6 {
+        font-family: 'Big Shoulders Text', cursive;
+        font-weight: bold;
+        letter-spacing: 0.15rem;
+        line-height: 1.7;
+        margin: 1em 0 15px;
+        padding: 0;
+    }
+
+    h1 {
+        font-size: 36px;
+    }
+    h1 a {
+        color: inherit;
+    }
+
+    h2 {
+        font-size: 30px;
+    }
+
+    h3 {
+        font-size: 24px;
+    }
+
+    h4 {
+        font-size: 20px;
+    }
+
+    a, p {
+        font-family: Roboto;
+        font-size: 18px;
+        line-height: 1.7;
+        letter-spacing: 0.1rem;
+    }
+
+    p {
+        margin: 15px 0;
+    }
+
+    deckgo-highlight-code {
+        --deckgo-highlight-code-font-size: 18px;
+        --deckgo-highlight-code-token-comment: rgb(150,150,150);
+    }
+
+
+`;
+
+export default MikeDoesCodingCss;
